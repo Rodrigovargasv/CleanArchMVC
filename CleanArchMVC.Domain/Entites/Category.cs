936c95797bd8dc0035ee41a1c62f02757a7fb5ef
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-05-31 06:37:14 -0300

    Creating method for validating the category class

--- CleanArchMVC.Domain/Entites/Category.cs
+++ CleanArchMVC.Domain/Entites/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMVC.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,37 @@ namespace CleanArchMVC.Domain.Entites
         public string Name { get; private set; }
 
         public ICollection<Product> Products { get;}
+
+
+        // Constructor
+        public Category(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "The id not can be 0");
+            ValidationDomain(name);
+        }
+
+        // Method to update the domain
+        // Método para atualizar o domain
+        public void Update(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+               "The Name field is required");
+            DomainExceptionValidation.When(name.Length < 3,
+                "The Name field has at least 3 characters");
+
+            Name = name;
+            
+        }
 
     }
 }
