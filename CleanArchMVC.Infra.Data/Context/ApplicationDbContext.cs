b39914519c3eec29db3106e4a3ddbad2d212f10f
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-06-07 05:52:22 -0300

    Instantiated method to perform basic CRUD, and implementing the ApplicationDbContext class to map tables.

---
+++ CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchMVC.Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        // Maps product and category classes to tables
+        // Mapeia classes de produto e categoria para tabelas
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+        // Configures the Fluent API template
+        // Configura o modelo Fluent API
+
+        protected override void OnModelCreating(ModelBuilder optionsBuilder)
+        {
+            base.OnModelCreating(optionsBuilder);
+            optionsBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext)
+                .Assembly);
+        }
+    }
+}
---
+++ CleanArchMVC.Infra.Data/Repositories/CategoryService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMVC.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.Data.Repositories
+{
+    public class CategoryService : ICategoryRepository
+    {
+    }
+}
