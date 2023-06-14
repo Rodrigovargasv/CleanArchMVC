c99ddb3ff553e5ba058829e02199b7fb189080b2
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-06-14 06:22:17 -0300

    Created the ICategoryService and IProductService interfaces and configured interfaces to perform basic CRUD operations

---
+++ CleanArchMVC.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMVC.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetAllCategoryDTOsAsync();
+        Task<CategoryDTO> GetByIdCategoryDTOAsync(int? id);
+        Task<CategoryDTO> CreateCategoryDTO(CategoryDTO categoryDTO);
+        Task<CategoryDTO> UpdateCategoryDTOAsync(CategoryDTO categoryDTO);
+        Task<CategoryDTO> DeleteCategoryDTOAsync(int? id);
+    }
+}
---
+++ CleanArchMVC.Application/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMVC.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetAllProductDTOsAsync();
+        Task<ProductDTO> GetByIdProductDTOAsync(int? id);
+        Task<ProductDTO> CreateProductDTOAync(ProductDTO productDTO);
+        Task<ProductDTO> UpdateProductDTOAsync(ProductDTO productDTO);
+        Task<ProductDTO> DeleteProductDTOAsync(int? id);
+    }
+}
