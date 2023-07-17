79e25ba79692e484f5dc35d4d2c7ec074cfd66d0
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-07-17 11:09:48 -0300

    Was created the LoginViewModel and RegisterViewModel classes. Finally was added you properties

---
+++ CleanArchMVC.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchMVC.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMVC.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Register() { }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel model) { }
+
+        [HttpGet]
+        public ActionResult Login() {  }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model) { }
+
+
+        public async Task<ActionResult> Logout() { }
+        
+    }
+}
