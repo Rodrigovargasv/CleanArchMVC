43c1faab5d81a46b759606fddb0dd749d0118517
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-07-18 11:55:49 -0300

    Was implemented methods to perform the register and logout at the system

--- CleanArchMVC.WebUI/Controllers/AccountController.cs
+++ CleanArchMVC.WebUI/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.WebUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMVC.WebUI.Controllers
@@ -13,21 +14,65 @@ namespace CleanArchMVC.WebUI.Controllers
         }
 
 
-
         [HttpGet]
-        public IActionResult Register() { }
+        public ActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
 
         [HttpPost]
-        public async Task<ActionResult> Register(RegisterViewModel model) { }
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authenticate.Authenticate(model.Email, model.Password);
+
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.(password must be strong)");
+                return View(model);
+            }
+        }
+
+
 
         [HttpGet]
-        public ActionResult Login() {  }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [HttpPost]
-        public async Task<ActionResult> Login(LoginViewModel model) { }
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            var result = await _authenticate.RegisterUser(model.Email, model.Password);
+
+            if (result)
+                return Redirect("/");
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid register attempt (password must be strong.)");
+                return View(model);
+            }
+
+        }
+
+        public async Task<ActionResult> Logout()
+        {
+            await _authenticate.Logout();
+            return Redirect("/Account/login");
+        }
 
 
-        public async Task<ActionResult> Logout() { }
-        
+
     }
 }
